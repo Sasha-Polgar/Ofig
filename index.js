651131d8a0157b441cef5dbdd68c03bda62dfec1
Sasha-Polgar <sasha.polgar@hotmail.com>
2023-08-30 10:21:18 +0200

    étape 2: factorisation des views

--- index.js
+++ index.js
@@ -12,11 +12,16 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Configure view engine
+app.set("view engine", "ejs");
+app.set("views", "./app/views");
+
 // servir les fichiers statiques qui sont dans "integration"
 app.use(express.static('integration'));
 
 // routage !
 app.use(router);
+
 
 
 // on lance le serveur
